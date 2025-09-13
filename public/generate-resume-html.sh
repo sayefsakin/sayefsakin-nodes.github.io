8f29326a5a62061a36bb7a417acca2b1d26d6661
Sayef Azad Sakin <sayefsakin@yahoo.co.in>
2025-09-12 18:37:45 -0600

    CV formatting

--- public/generate-resume-html.sh
+++ public/generate-resume-html.sh
@@ -1,5 +1,6 @@
 rm resume.html
+cp ../../sayefsakin.github.io/sayefsakin.github.io/assets/data/resume.json .
 cd ../../jsonresume-theme-sayef-academic/jsonresume-theme-sayef-academic/
 sudo npm install -g .
-cd ../../personal-web-demo/public/
+cd ../../sayefsakin-nodes.github.io/public/
 resumed render -t jsonresume-theme-sayef-academic -o resume.html
